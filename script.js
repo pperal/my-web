6a41c8a897e70e9f79f008dc1ad81ca3acadea06
ppcyel <42583572+ppcyel@users.noreply.github.com>
2021-02-12 00:55:47 +0700

    fix

--- script.js
+++ script.js
@@ -15,11 +15,38 @@ gsap.to(sections, {
             delay: 0
         },
 
-        end: () => "+=" + (document.querySelector(".container").offsetWidth / 3)
+        end: () => "+=" + (document.querySelector(".container").offsetWidth / 2)
     }
 });
 
+//////////-------------------------yoyo
 
+
+let config = { strength: 1 };
+
+gsap.set("h1", { xPercent: -50, x: -1 });
+
+gsap.to("h1", {
+    repeat: -1,
+    yoyo: true,
+    x: 1,
+    duration: 0.2,
+    ease: "power1.inOut",
+    modifiers: {
+        x: gsap.utils.unitize(value => value * config.strength, "px")
+    }
+});
+
+gsap.to(config, {
+    strength: 100,
+    ease: "none",
+    scrollTrigger: {
+        // defaults to using the window as the trigger, starting at the top, ending at the bottom.
+        scrub: true
+    }
+});
+
+//-------------------------------------------------
 
 
 
